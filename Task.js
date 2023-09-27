acab75a30034e3a88fef70bfe02cff85b5c93c43
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-09-27 12:56:45 +0600

    Create 3 Constructor Functions, Show Task and Reset Form Part Done

---
+++ Task.js
@@ -0,0 +1,8 @@
+
+function Task(title) {
+  this.id = new Date().toLocaleString();
+  this.title = title;
+  this.isCompleted = false;
+}
+
+export default Task;
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+
+import UI from './UI.js';
+import Task from './Task.js';
+
+
+
+const ui = new UI();
+
+document.querySelector('.AddTaskBtn').addEventListener('click', (e) => {
+  const taskTitle = document.querySelector('#newtaskID').value;
+  const task = new Task(taskTitle);
+  ui.addToUI(task);
+  ui.resetForm();
+
+});
+
